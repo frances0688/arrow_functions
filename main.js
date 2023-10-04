5c21bbe6ce8a59fb16654a9dbbf94f0e5f843c2a
Frances Morales Velilla <frances0688@gmail.com>
2023-10-04 16:02:50 +0200

    all extra  exercises completed

--- main.js
+++ main.js
@@ -113,7 +113,7 @@ const foodList = [
     { name: 'Cheesbacon burguer', isVeggie: false },
     { name: 'Tofu burguer', isVeggie: true },
     { name: 'Entrecot', isVeggie: false }
-    ];
+];
 const veggieFoods = foodList.filter(food => food.isVeggie === true).map(food => `Que rico ${food.name} me voy a comer!`);
 console.log(veggieFoods)
 
@@ -125,3 +125,96 @@ console.log(veggieFoods)
 const numeros1 = [39, 2, 4, 25, 62];
 const numerosMult = numeros1.reduce((a,b) => a*b)
 console.log(numerosMult)
+
+
+// Extras
+
+
+// Map
+
+// Dado el siguiente array, crear un segundo array que forme frases como en 
+//el ejemplo accediendo a las propiedades del objeto proporcionado:
+// Resultado esperado
+/*
+[
+    'Pepe es TheBoss y le gusta leer y ver pelis',
+    'Ana es becaria y le gusta nadar y bailar',
+    'Luis es programador y le gusta dormir y comprar',
+    'Ana es becaria y le gusta nadar y bailar',
+    'Carlos es secretario y le gusta fútbol y queso'
+]
+*/
+const staff = [
+  {name: "Pepe", role: "The Boss", hobbies: ["leer", "ver pelis"]},
+  {name: "Ana", role: "becaria", hobbies: ["nadar", "bailar"]},
+  {name: "Luis", role: "programador", hobbies: ["dormir", "comprar"]},
+  {name: "Carlos", role: "secretario", hobbies: ["futbol", "queso"]}
+];
+const staffPhrase = staff.map(member => `${member.name} es ${member.role} y le gusta ${member.hobbies[0]} y ${member.hobbies[1]}`)
+console.log(staffPhrase)
+
+// Dado el siguiente array, generar un segundo array que consiga generar de 
+//salida el resultado esperado:
+//Resultado esperado
+/* [
+    'Como soy de Italia, amo comer Pizza',
+    'Como soy de Japón, amo comer Ramen',
+    'Como soy de Valencia, amo comer Paella',
+    'Aunque no como carne, el Entrecot es sabroso'
+]
+*/
+const foodList1 = ["Pizza", "Ramen", "Paella", "Entrecot"];
+const foodPhrase = foodList1.map(food => {
+    switch (food) {
+        case "Pizza": 
+            return `Como soy de Italia, amo comer ${food}`;
+        case "Ramen":
+            return `Como soy de Japón, amo comer ${food}`;
+        case "Paella":
+            return `Como soy de Valencia, amo comer ${food}`;
+        default:
+            return `Aunque no como carne, el ${food} es sabroso`;
+    }
+})
+console.log(foodPhrase)
+
+
+// Filter
+
+// Dado el siguiente array, devolver un array con los nombres de los elementos 
+//que valgan más de 300 euros
+/*
+[
+    'TV Samsung,',
+    'Viaje a Cancún'
+]
+*/
+const inventory = [
+    {name: 'Mobile phone', price: 199},
+    {name: 'TV Samsung', price: 459},
+    {name: 'Viaje a cancún', price: 600},
+    {name: 'Mascarilla', price: 1}
+];
+const filterInventory = inventory.filter(item => item.price > 300).map(item => item.name)
+console.log(filterInventory)
+
+
+// Reduce
+
+// Concatena todos los elementos del array con reduce para que devuelva una 
+//sola frase
+// Resultado--> 'Me llamo XX y quiero sentir la fuerza con javascript'
+const sentenceElements = [
+    'Me',
+    'llamo',
+    'Frances' /* Tu nombre aquí! */,
+    'y',
+    'quiero',
+    'sentir',
+    'la',
+    'fuerza',
+    'con',
+    'javascript'
+];
+const sentence = sentenceElements.reduce((a,b) => a +' '+ b);
+console.log(sentence)
